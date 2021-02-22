cd46881b07450adf236417eac2c12afa52bc452b
xiaoman <ygyuanman@163.com>
2021-02-22 18:06:27 +0800

    版本2提交

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div id="app">
     <h1>拉钩教育</h1>
     <router-view/>
+<el-row>
+  <el-button type="danger">按钮</el-button>
+</el-row>
   </div>
 </template>
 
 <style lang="scss">
+div{
+color:$warning-color;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element
+import ElementUI from 'element-ui'
+// // 引入 element 的主题文件
+// import 'element-ui/lib/theme-chalk/index.css'
+// 引入 自定义的全局样式文件
+import './styles/index.scss'
+// 将element注册为vue插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
